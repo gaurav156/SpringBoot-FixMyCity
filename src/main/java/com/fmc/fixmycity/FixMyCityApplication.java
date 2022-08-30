17ace6cd8c5232f1b8b47321a254e44143985b95
gaurav.shukla <gauravshukla156@gmail.com>
2022-08-30 21:57:18 +0530

    Adds API Key, Initializes Firebase & Sets Up Port Number

--- src/main/java/com/fmc/fixmycity/FixMyCityApplication.java
+++ src/main/java/com/fmc/fixmycity/FixMyCityApplication.java
@@ -1,12 +1,31 @@
 package com.fmc.fixmycity;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class FixMyCityApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		ClassLoader classLoader = FixMyCityApplication.class.getClassLoader();
+
+		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+		FileInputStream serviceAccount = new FileInputStream(file.getAbsoluteFile());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+
+		FirebaseApp.initializeApp(options);
 		SpringApplication.run(FixMyCityApplication.class, args);
 	}
 
