b9a6f5682a53824f7e5974e17faef38a7784bc92
gaurav.shukla <gauravshukla156@gmail.com>
2022-08-30 19:06:00 +0530

    Adds Firebase dependency

---
+++ src/main/java/com/fmc/fixmycity/FixMyCityApplication.java
@@ -0,0 +1,13 @@
+package com.fmc.fixmycity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixMyCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixMyCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmc/fixmycity/FixMyCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmc.fixmycity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMyCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
